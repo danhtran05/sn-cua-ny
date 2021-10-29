bf616c131648236a869c070de174fc946f71d575
unknown <you@example.com>
2021-10-29 22:36:27 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Cobe m52';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng người bạn cùng bàn';
+const contentCard = 'Bạn cùng bàn sinh nhật 🎂 zui zẻ .Chúc bạn ngày càng học giỏi và ngày xinh đẹp Học giỏi để còn gánh mình😂';
+const giftImage = 'ny.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
